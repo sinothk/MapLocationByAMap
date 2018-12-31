89cf85ddf482f9adc23240855f7e0c74fd117e84
LiangYT <381518188@qq.com>
2018-12-31 23:04:21 +0800

    同步

---
+++ app/src/main/java/com/sinothk/map/amap/location/demo/LocationDemoMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.map.amap.location.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationDemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_demo_main);
+    }
+}
