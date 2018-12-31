165c08ed3da75c26bc6cf4b8c2d4374515d414ce
LiangYT <381518188@qq.com>
2019-01-01 00:02:56 +0800

    实现定位工具类

---
+++ MapLocationByAMap/src/main/java/com/sinothk/map/amap/location/AMapLocation.java
@@ -0,0 +1,148 @@
+package com.sinothk.map.amap.location;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * <pre>
+ *  创建:  梁玉涛 2018/12/31 on 23:17
+ *  项目:  MapLocationByAMapLib
+ *  描述:
+ *  更新:
+ * <pre>
+ */
+public class AMapLocation {
+
+    /**
+     * 单次定位
+     *
+     * @param mContext
+     */
+    public static void location(Context mContext) {
+
+        //声明mlocationClient对象
+        AMapLocationClient locationClient = new AMapLocationClient(mContext);
+
+        //声明mLocationOption对象
+        AMapLocationClientOption mLocationOption = new AMapLocationClientOption();
+
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+//        //设置定位间隔,单位毫秒,默认为2000ms
+//        mLocationOption.setInterval(2000);
+//
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+
+        //设置定位参数
+        locationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+
+        //设置定位监听
+        locationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(com.amap.api.location.AMapLocation amapLocation) {
+                if (amapLocation != null) {
+                    if (amapLocation.getErrorCode() == 0) {
+                        //定位成功回调信息，设置相关消息
+                        amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        amapLocation.getLatitude();//获取纬度
+                        amapLocation.getLongitude();//获取经度
+                        amapLocation.getAccuracy();//获取精度信息
+                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        Date date = new Date(amapLocation.getTime());
+                        df.format(date);//定位时间
+
+                    } else {
+                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError", "location Error, ErrCode:"
+                                + amapLocation.getErrorCode() + ", errInfo:"
+                                + amapLocation.getErrorInfo());
+                    }
+                }
+            }
+        });
+
+        //启动定位
+        locationClient.startLocation();
+    }
+
+    /**
+     * 连续定位
+     *
+     * @param mContext
+     * @param second
+     */
+    public static void locationContinue(Context mContext, int second) {
+
+        //声明mlocationClient对象
+        AMapLocationClient locationClient = new AMapLocationClient(mContext);
+
+        //声明mLocationOption对象
+        AMapLocationClientOption mLocationOption = new AMapLocationClientOption();
+
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(second * 1000);
+//
+//        // ===================== 获取一次定位结果：
+//        //该方法默认为false。
+//        mLocationOption.setOnceLocation(true);
+//        //获取最近3s内精度最高的一次定位结果：
+//        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+//        mLocationOption.setOnceLocationLatest(true);
+
+        //设置定位参数
+        locationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+
+        //设置定位监听
+        locationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(com.amap.api.location.AMapLocation amapLocation) {
+                if (amapLocation != null) {
+                    if (amapLocation.getErrorCode() == 0) {
+                        //定位成功回调信息，设置相关消息
+                        amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        amapLocation.getLatitude();//获取纬度
+                        amapLocation.getLongitude();//获取经度
+                        amapLocation.getAccuracy();//获取精度信息
+                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        Date date = new Date(amapLocation.getTime());
+                        df.format(date);//定位时间
+
+                    } else {
+                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError", "location Error, ErrCode:"
+                                + amapLocation.getErrorCode() + ", errInfo:"
+                                + amapLocation.getErrorInfo());
+                    }
+                }
+            }
+        });
+
+        //启动定位
+        locationClient.startLocation();
+    }
+}
--- app/src/main/java/com/sinothk/map/amap/location/demo/LocationDemoMainActivity.java
+++ app/src/main/java/com/sinothk/map/amap/location/demo/LocationDemoMainActivity.java
@@ -3,11 +3,15 @@ package com.sinothk.map.amap.location.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sinothk.map.amap.location.AMapLocation;
+
 public class LocationDemoMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_demo_main);
+
+        AMapLocation.location(this);
     }
 }
